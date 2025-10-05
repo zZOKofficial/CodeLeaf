6d23cbe256131d52b483002ccbd127b9a179022a
zZOKofficial <zzokofficial@gmail.com>
2025-10-05 10:57:58 +0600

    skeleton push

---
+++ app/src/main/java/com/zzok/codeleaf/MainActivity.java
@@ -0,0 +1,63 @@
+package com.zzok.codeleaf;
+
+import android.animation.*;
+import android.app.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.Typeface;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.os.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.widget.*;
+import androidx.annotation.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import com.zzok.codeleaf.databinding.*;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+import org.json.*;
+
+public class MainActivity extends AppCompatActivity {
+	
+	private MainBinding binding;
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		binding = MainBinding.inflate(getLayoutInflater());
+		setContentView(binding.getRoot());
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		
+		binding.codeleafTitle.setOnLongClickListener(new View.OnLongClickListener() {
+			@Override
+			public boolean onLongClick(View _view) {
+				SketchwareUtil.showMessage(getApplicationContext(), "If you have found this, man, get a job!");
+				return true;
+			}
+		});
+	}
+	
+	private void initializeLogic() {
+		binding.codeleafTitle.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/display_bold.ttf"), 0);
+		binding.subtitle.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/bangla_bold.ttf"), 0);
+		binding.credit.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/mono_bold.ttf"), 0);
+	}
+	
+}
