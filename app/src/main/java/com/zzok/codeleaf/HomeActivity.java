70635e7bfe68ddb8aea81c13c616882134b3dad2
zZOKofficial <zzokofficial@gmail.com>
2025-10-08 00:25:46 +0600

    v0.3.0 UI Overhaul

--- app/src/main/java/com/zzok/codeleaf/HomeActivity.java
+++ app/src/main/java/com/zzok/codeleaf/HomeActivity.java
@@ -3,17 +3,20 @@ package com.zzok.codeleaf;
 import android.animation.*;
 import android.app.*;
 import android.content.*;
+import android.content.Intent;
 import android.content.res.*;
 import android.graphics.*;
 import android.graphics.Typeface;
 import android.graphics.drawable.*;
 import android.media.*;
 import android.net.*;
+import android.net.Uri;
 import android.os.*;
 import android.text.*;
 import android.text.style.*;
 import android.util.*;
 import android.view.*;
+import android.view.View;
 import android.view.View.*;
 import android.view.animation.*;
 import android.webkit.*;
@@ -34,6 +37,8 @@ public class HomeActivity extends AppCompatActivity {
 	
 	private HomeBinding binding;
 	
+	private Intent intent = new Intent();
+	
 	@Override
 	protected void onCreate(Bundle _savedInstanceState) {
 		super.onCreate(_savedInstanceState);
@@ -44,11 +49,51 @@ public class HomeActivity extends AppCompatActivity {
 	}
 	
 	private void initialize(Bundle _savedInstanceState) {
+		
+		binding.linearHtml.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				intent.setClass(getApplicationContext(), HtmlActivity.class);
+				startActivity(intent);
+			}
+		});
+		
+		binding.linearCss.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				intent.setClass(getApplicationContext(), CssActivity.class);
+				startActivity(intent);
+			}
+		});
+		
+		binding.linearPhp.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				intent.setClass(getApplicationContext(), PhpActivity.class);
+				startActivity(intent);
+			}
+		});
+		
+		binding.linearSql.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				intent.setClass(getApplicationContext(), SqlActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 	
 	private void initializeLogic() {
 		binding.codeleafText.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/display_bold.ttf"), 0);
 		binding.catalog.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/bangla_bold.ttf"), 0);
+		binding.htmlTitle.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/bangla_bold.ttf"), 0);
+		binding.htmlSubtitle.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/bangla_regular.ttf"), 0);
+		binding.cssTitle.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/bangla_bold.ttf"), 0);
+		binding.cssSubtitle.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/bangla_regular.ttf"), 0);
+		binding.phpTitle.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/bangla_bold.ttf"), 0);
+		binding.phpSubtitle.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/bangla_regular.ttf"), 0);
+		binding.sqlTitle.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/bangla_bold.ttf"), 0);
+		binding.sqlSubtitle.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/bangla_regular.ttf"), 0);
 	}
 	
 }
